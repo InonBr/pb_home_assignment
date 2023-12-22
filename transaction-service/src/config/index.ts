d05536d10e13aaa27e794779cb3b4c05e5bcee38
Inon Bercu <inonbr@gmail.com>
2023-12-22 17:33:32 +0200

    created docker file for transaction_service_server

--- transaction-service/src/config/index.ts
+++ transaction-service/src/config/index.ts
@@ -3,7 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const port = process.env.TRANSACTIONS_PORT || "5002";
-export const userServiceUrl = process.env.USER_SERVICE_URL || "";
+export const userServiceUrl = process.env.TRANSACTIONS_ENV
+  ? process.env.USER_SERVICE_DOCKER_URL
+  : process.env.USER_SERVICE_URL;
 export const connectionString = process.env.TRANSACTIONS_ENV
   ? process.env.TRANSACTIONS_DOCKER_KEY
   : process.env.TRANSACTIONS_MONGO_KEY;
